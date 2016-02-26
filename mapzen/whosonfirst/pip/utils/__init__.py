0a1a517854efb2d043fca578d361b7cbf1d450e0
Dan Phiffer <dan@phiffer.org>
2016-02-26 17:24:22 -0500

    🐙 More helpers, start using The Cloud™

--- mapzen/whosonfirst/pip/utils/__init__.py
+++ mapzen/whosonfirst/pip/utils/__init__.py
@@ -16,34 +16,45 @@ def append_hierarchy_and_parent(feature, **kwargs):
 
     lat = props.get('lbl:latitude', None)
     lon = props.get('lbl:longitude', None)
-    
+
     if not lat or not lon:
         lat = props.get('geom:latitude', None)
         lon = props.get('geom:longitude', None)
-        
+
     if not lat or not lon:
 
         shp = shapely.geometry.asShape(feature['geometry'])
         coords = shp.centroid
-        
+
         lat = coords.y
         lon = coords.x
-        
+
+    endpoint = kwargs.get('data_endpoint', 'https://whosonfirst.mapzen.com/data/')
+    reverse_geocoded = get_reverse_geocoded(lat, lon, placetype)
+    props['wof:parent_id'] = get_parent(reverse_geocoded, lat, lon)
+    props['wof:hierarchy'] = get_hierarchy(reverse_geocoded, data_endpoint)
+
+    feature['properties'] = props
+
+    return True
+
+def get_reverse_geocoded(lat, lon, placetype):
+
     # see also : https://github.com/whosonfirst/go-whosonfirst-pip#wof-pip-server
-    pip = mapzen.whosonfirst.pip.proxy()
+    pip = mapzen.whosonfirst.pip.proxy(hostname='pip.mapzen.com')
 
     pt = mapzen.whosonfirst.placetypes.placetype(placetype)
-    
+
     _hiers = []
     _rsp = []
-    
+
     for parent in pt.parents():
-        
+
         parent = str(parent)
 
         # TO DO: some kind of 'ping' to make sure the server is actually
         # there... (20151221/thisisaaronland)
-        
+
         # print "%s : %s,%s" % (parent, lat, lon)
 
         try:
@@ -56,12 +67,31 @@ def append_hierarchy_and_parent(feature, **kwargs):
             _rsp = rsp
             break
 
-    for r in _rsp:
+    return _rsp
+
+def get_parent(reverse_geocoded, lat, lon):
+
+    parent_id = -1
+
+    if len(reverse_geocoded) == 0:
+        logging.warning("Failed to reverse geocode any parents for %s, %s" % (lat, lon))
+    elif len(reverse_geocoded) > 1:
+        logging.warning("Multiple reverse geocoding possibilities %s, %s" % (lat, lon))
+    else:
+        parent_id = reverse_geocoded[0]['Id']
+
+    return parent_id
+
+def get_hierarchy(reverse_geocoded, data_endpoint):
+
+    for r in reverse_geocoded:
         id = r['Id']
-        pf = mapzen.whosonfirst.utils.load(kwargs.get('data_root', ''), id)
+        #pf = mapzen.whosonfirst.utils.load(kwargs.get('data_root', ''), id)
+        rsp = requests.get(data_endpoint + id2relpath(id))
+        pf = json.loads(rsp.content)
         pp = pf['properties']
         ph = pp['wof:hierarchy']
-            
+
         for h in ph:
 
             h[ "%s_id" % placetype ] = wofid
@@ -70,18 +100,4 @@ def append_hierarchy_and_parent(feature, **kwargs):
             # h[k] = wofid
             _hiers.append(h)
 
-    parent_id = -1
-
-    if len(_rsp) == 0:
-        logging.warning("Failed to reverse geocode any parents for %s, %s" % (lat, lon))
-    elif len(_rsp) > 1:  
-        logging.warning("Multiple reverse geocoding possibilities %s, %s" % (lat, lon))
-    else:
-        parent_id = _rsp[0]['Id']
-
-    props['wof:parent_id'] = parent_id
-    
-    props['wof:hierarchy'] = _hiers
-    feature['properties'] = props
-
-    return True
+    return _hiers
